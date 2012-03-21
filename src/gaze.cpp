1bab6f678fc683072b61543ab56e1d0969ff3073
Jasper van Turnhout <jturnhout@gmail.com>
2012-03-21 14:38:37 +0100

    mood detector integration

--- src/gaze.cpp
+++ src/gaze.cpp
@@ -157,10 +157,13 @@ int main (int argc, char *argv[])
                 genderString << "Gender: " << ((people.at(i).getGender())==-1?"male":"female");
                 std::ostringstream ageString;
                 ageString << "Age: " << people.at(i).getAge();
+                std::ostringstream moodString;
+                moodString << "Mood" << people.at(i).getMood();
                 cv::putText(frame, idString.str(),cv::Point(face.x+10,face.y+20),cv::FONT_HERSHEY_SIMPLEX, 0.5, colors[people.at(i).getBestMatch()%8]);
                 cv::putText(frame, genderString.str(), cv::Point(face.x+10,face.y+40),cv::FONT_HERSHEY_SIMPLEX, 0.5, colors[people.at(i).getBestMatch()%8]);
                 cv::putText(frame, ageString.str(), cv::Point(face.x+10,face.y+60),cv::FONT_HERSHEY_SIMPLEX, 0.5, colors[people.at(i).getBestMatch()%8]);
                 cv::putText(frame, ageString.str(), cv::Point(face.x+10,face.y+60),cv::FONT_HERSHEY_SIMPLEX, 0.5, colors[people.at(i).getBestMatch()%8]);
+                cv::putText(frame, moodString.str(), cv::Point(face.x+10,face.y+80),cv::FONT_HERSHEY_SIMPLEX, 0.5, colors[people.at(i).getBestMatch()%8]);
 
                 if (people.at(i).getGender()==-1)
                 {
